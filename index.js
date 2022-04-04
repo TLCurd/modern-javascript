4763019ad43fb427f7cfeee3c13bae8a30bab3c4
TLCurd <TLCurdChiro@gmail.com>
2022-04-04 13:20:33 -0600

    add math library and examples

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 /* global moment */
 var moment = require('moment');
+const math = require('mathjs');
 console.log("hola");
 console.log(moment().format("[Today is] dddd"));
 console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 // Interpolate variable bindings
-var name = "Bob", time = "today";
+var name = "Yolanda", time = "right now";
 console.log(`Hello ${name}, how are you ${time}?`);
 
+console.log("what is happening right now");
+
+console.log(44 * 3);
+
+console.log(math.multiply(5,3, 3));
+
+console.log('chained operations');
+const a = math.chain(3)
+  .add(4)
+  .multiply(2)
+  .done();
+console.log(a); // 14
+console.log();
