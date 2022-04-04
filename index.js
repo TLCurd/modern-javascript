36edb2eddf2fcb8cc15df884d3962bea4d7f306e
TLCurd <TLCurdChiro@gmail.com>
2022-04-04 12:30:02 -0600

    add webpack

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+/* global moment */
+var moment = require('moment');
 console.log("hola");
+console.log(moment().format("[Today is] dddd"));
+console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
